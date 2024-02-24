5cb78a30ffa85aec66ef63e1c2ab1f33013eba0b
yada <trojblue@gmail.com>
2024-02-24 00:58:27 +0000

    fix: validate images before trying to open to gracefully handle broken images

--- ml_danbooru_tagger/data/path_dataset.py
+++ ml_danbooru_tagger/data/path_dataset.py
@@ -1,16 +1,31 @@
 from torch.utils.data import Dataset
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 class PathDataset(Dataset):
     def __init__(self, img_list, transform=None):
         self.img_list = img_list
         self.transform = transform
+        self.valid_indices = self._validate_images()
+
+    def _validate_images(self):
+        valid_indices = []
+        for i, path in enumerate(self.img_list):
+            try:
+                with Image.open(path) as img:
+                    img.verify()  # Verify the image to check if it's corrupted
+                valid_indices.append(i)
+            except (IOError, UnidentifiedImageError):
+                print(f"Skipping corrupted image: {path}")
+        return valid_indices
 
     def __getitem__(self, index):
-        path = self.img_list[index]
-        img = Image.open(path).convert('RGB')
-        img = self.transform(img)
+        valid_index = self.valid_indices[index]
+        path = self.img_list[valid_index]
+        with Image.open(path) as img:
+            img = img.convert('RGB')
+            if self.transform:
+                img = self.transform(img)
         return img, path
 
     def __len__(self):
-        return len(self.img_list)
+        return len(self.valid_indices)
